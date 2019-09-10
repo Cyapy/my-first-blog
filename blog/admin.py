ec94ef330634377a2f6844de5989ad0cb594c970
Cyapy <cz.arnold1977@gmail.com>
2019-09-10 12:59:43 +0200

    My Django Girls app, first commit

---
+++ blog/admin.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+
+#from .models import Company
+from .models import Post
+
+#admin.site.register(Company)
+admin.site.register(Post)
+
---
+++ python_intro.py
@@ -0,0 +1,21 @@
+def welcome():
+	print('You Are')
+	print('Welcome')
+	print('  !!!')
+	print('')
+	
+
+
+welcome()
+
+x = 13
+
+if x>5 : 
+	print(x,'greater than 5')
+else:
+	print('not greater')
+
+
+	
+	
+# this is a smart comment here
